86e0070e8cf4556c4a151d3fa2897d87bd9b369e
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2024-11-14 17:20:38 -0300

    Integrating the success screen with the flow

--- lib/src/presentation/settings/success/onboarding_success_screen.dart
+++ lib/src/presentation/settings/success/onboarding_success_screen.dart
@@ -1,8 +1,23 @@
+import 'package:ambush_app/src/core/routes/app_route.gr.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:ambush_app/src/designsystem/buttons.dart';
+import 'package:ambush_app/src/presentation/add_invoice/add_invoice_navigation_flow.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class OnboardingSuccessScreen extends StatelessWidget {
   const OnboardingSuccessScreen({super.key});
+
+  void _onAddClick(BuildContext context) {
+    final navigator = context.router;
+    navigator.replace(InvoiceListRoute());
+    final flow = AddInvoiceNavigationFlow(navigator);
+    flow.start();
+  }
+
+  void _willDoThisLater(BuildContext context) {
+    context.router.replaceAll([InvoiceListRoute()]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +49,7 @@ class OnboardingSuccessScreen extends StatelessWidget {
                   'Success!',
                   style: textTheme.titleLarge,
                 ),
-                SizedBox(
-                    height: 8),
+                SizedBox(height: 8),
                 Text(
                   "All set! YOUR company's information has been\nsuccessfully submitted. We're ready to move\nforward with your invoice!",
                   textAlign: TextAlign.start, // Align left for subtitle
@@ -48,7 +62,6 @@ class OnboardingSuccessScreen extends StatelessWidget {
               height: 120,
               width: 120,
               decoration: BoxDecoration(
-
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(16),
               ),
@@ -61,13 +74,13 @@ class OnboardingSuccessScreen extends StatelessWidget {
             ),
             SizedBox(height: 64.0),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
                     onPressed: () {
-                      
+                      _willDoThisLater(context);
                     },
                     child: Text(
                       "I'll do it later",
@@ -77,13 +90,12 @@ class OnboardingSuccessScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(width: 16.0,),
-                  PrimaryButton(
-                    text: "Create Invoice",
-                    onPressed: () {
-                      
-                    }
+                  SizedBox(
+                    width: 16.0,
                   ),
+                  PrimaryButton(text: "Create Invoice", onPressed: () {
+                    _onAddClick(context);
+                  }),
                 ],
               ),
             ),
