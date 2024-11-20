6d603fe37b93c2bba75491cc74e7da13e258d113
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2024-11-20 17:36:17 -0300

    updated gitignore and removed autogenerated files

--- test/src/data/repository/application_backup_repository_test.dart
+++ test/src/data/repository/application_backup_repository_test.dart
@@ -12,10 +12,15 @@ import 'application_backup_repository_test.mocks.dart';
 void main() {
   late MockILocalDataSource mockLocalDataSource;
   late ApplicationBackupRepository sut;
+
+  setUp() {
+    mockLocalDataSource = MockILocalDataSource();
+    sut = ApplicationBackupRepository(mockLocalDataSource);
+  }
+
   group('ApplicationBackupRepository', () {
+    setUp();
     test('test get - should save info', () {
-      mockLocalDataSource = MockILocalDataSource();
-      sut = ApplicationBackupRepository(mockLocalDataSource);
       when(mockLocalDataSource.getCompanyInfo()).thenReturn(mockCompanyInfo);
       when(mockLocalDataSource.getClientInfo()).thenReturn(mockClientInfo);
       when(mockLocalDataSource.getServiceInfo()).thenReturn(mockServiceInfo);
@@ -27,8 +32,7 @@ void main() {
     });
 
     test('test save - should send correct hive models', () async {
-      mockLocalDataSource = MockILocalDataSource();
-      sut = ApplicationBackupRepository(mockLocalDataSource);
+      setUp();
       await sut.save(
           '{"clientInfo":{"name":"Name","address":"Address"},"serviceInfo":{"description":"Description","quantity":0.0,"currencyName":"Currency","currencySymbol":"Symbol","currencyCC":"CC","price":10.0},"bankInfo":{"beneficiaryName":"Name","iban":"123","swift":"123","bankName":"123","bankAddress":"Address","intermediaryIban":null,"intermediarySwift":null,"intermediaryBankName":null,"intermediaryBankAddress":null},"companyInfo":{"name":"Name","address":{"street":"St","extraInfo":null,"neighbourhood":"Neighbourhood","city":"City","state":"State","country":"Country","zipCode":"Zip code"},"email":"email@email.com","ownerName":"Owner","cnpj":"123"},"invoiceList":[{"id":1,"issueDate":1,"dueDate":1,"createdAt":1,"updatedAt":1,"serviceInfo":{"description":"Description","quantity":0.0,"currencyName":"Currency","currencySymbol":"Symbol","currencyCC":"CC","price":10.0},"companyInfo":{"name":"Name","address":{"street":"Street","extraInfo":null,"neighbourhood":"Neighbourhood","city":"City","state":"State","country":"country","zipCode":"zipCode"},"email":"email@email.com","ownerName":"ownerName","cnpj":"cnpj"},"clientInfo":{"name":"name","address":"address"},"bankInfo":{"beneficiaryName":"beneficiaryName","iban":"iban","swift":"swift","bankName":"bankName","bankAddress":"bankAddress","intermediaryIban":null,"intermediarySwift":null,"intermediaryBankName":null,"intermediaryBankAddress":null}}]}');
       verify(mockLocalDataSource.saveBankInfo(any)).called(1);
