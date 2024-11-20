99f78159fb57c1817df1f34dc9aa54c2c5f88865
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2024-11-20 14:09:31 -0300

    created application backup repository

---
+++ lib/src/data/repositories/application_backup_repository.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:ambush_app/src/core/settings/const.dart';
+import 'package:ambush_app/src/data/datasource/local_datasource.dart';
+import 'package:ambush_app/src/data/models/hive_application_data.dart';
+import 'package:ambush_app/src/data/models/hive_invoice.dart';
+import 'package:ambush_app/src/presentation/utils/backup_error.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class IApplicationBackupRepository {
+  String get();
+  Future<void> save(String backup);
+}
+
+@Injectable(as: IApplicationBackupRepository)
+class ApplicationBackupRepository implements IApplicationBackupRepository {
+  final ILocalDataSource _localDataSource;
+
+  ApplicationBackupRepository(this._localDataSource);
+
+  @override
+  String get() {
+    final applicationData = HiveApplicationData(
+        _localDataSource.getClientInfo(),
+        _localDataSource.getServiceInfo(),
+        _localDataSource.getBankInfo(),
+        _localDataSource.getCompanyInfo(),
+        _localDataSource
+            .getInvoiceList()
+            .map((e) => HiveInvoice.fromInvoice(e))
+            .toList());
+    try {
+      final backup = jsonEncode(applicationData);
+      return backup;
+    } catch (_) {
+      throw BackupError(formatErrorMessage);
+    }
+  }
+
+  @override
+  Future<void> save(String backup) async {
+    try {
+      final HiveApplicationData applicationData = jsonDecode(backup);
+      final clientInfo = applicationData.clientInfo;
+      final serviceInfo = applicationData.serviceInfo;
+      final bankInfo = applicationData.bankInfo;
+      final companyInfo = applicationData.companyInfo;
+      final invoiceList = applicationData.invoiceList;
+
+      if (clientInfo != null) {
+        await _localDataSource.saveClientInfo(clientInfo);
+      }
+      if (serviceInfo != null) {
+        await _localDataSource.saveServiceInfo(serviceInfo);
+      }
+      if (bankInfo != null) {
+        await _localDataSource.saveBankInfo(bankInfo);
+      }
+      if (companyInfo != null) {
+        await _localDataSource.saveCompanyInfo(companyInfo);
+      }
+      for (var invoice in invoiceList) {
+        _localDataSource.saveInvoice(invoice.toInvoice());
+      }
+    } catch (_) {
+      throw BackupError(formatErrorMessage);
+    }
+  }
+}
