053f5bcf2e2218a0dfe6fe5ef23c08332fbc40f8
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2024-11-21 11:12:18 -0300

    removed hive dependency from application data

--- lib/src/data/models/hive_application_data.dart
+++ lib/src/data/models/hive_application_data.dart
@@ -4,13 +4,17 @@ import 'package:ambush_app/src/data/models/hive_company_info.dart';
 import 'package:ambush_app/src/data/models/hive_invoice.dart';
 import 'package:ambush_app/src/data/models/hive_service_info.dart';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import 'package:hive/hive.dart';
 >>>>>>> 7254d6d (unit tests)
+=======
+>>>>>>> 5de88e6 (removed hive dependency from application data)
 import 'package:json_annotation/json_annotation.dart';
 
 part 'hive_application_data.g.dart';
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 @JsonSerializable()
 class ApplicationData {
@@ -20,6 +24,10 @@ class ApplicationData {
 class HiveApplicationData extends HiveObject {
   @HiveField(0)
 >>>>>>> 7254d6d (unit tests)
+=======
+@JsonSerializable()
+class ApplicationData {
+>>>>>>> 5de88e6 (removed hive dependency from application data)
   HiveClientInfo? clientInfo;
 
   HiveServiceInfo? serviceInfo;
@@ -34,6 +42,7 @@ class HiveApplicationData extends HiveObject {
       this.companyInfo, this.invoiceList);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   factory ApplicationData.fromJson(Map<String, dynamic> json) =>
       _$ApplicationDataFromJson(json);
   Map<String, dynamic> toJson() => _$ApplicationDataToJson(this);
@@ -42,4 +51,9 @@ class HiveApplicationData extends HiveObject {
       _$HiveApplicationDataFromJson(json);
   Map<String, dynamic> toJson() => _$HiveApplicationDataToJson(this);
 >>>>>>> 7254d6d (unit tests)
+=======
+  factory ApplicationData.fromJson(Map<String, dynamic> json) =>
+      _$ApplicationDataFromJson(json);
+  Map<String, dynamic> toJson() => _$ApplicationDataToJson(this);
+>>>>>>> 5de88e6 (removed hive dependency from application data)
 }
