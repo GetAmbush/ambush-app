85ec6e3d9274c76310d5051b64fcca2f7c9814d1
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2024-11-26 10:31:13 -0300

    code review

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:ambush_app/src/core/utils/migration_utils.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
 import 'package:ambush_app/src/core/di/di.dart';
 import 'package:ambush_app/src/core/routes/app_route.gr.dart';
@@ -29,7 +28,7 @@ class MainPage extends StatelessWidget {
     return Container();
   }
 
-  void route(BuildContext context) async {
+  void route(BuildContext context) {
     if (_hasFinishedOnboarding.get()) {
       context.router.replace(InvoiceListRoute());
     } else {
